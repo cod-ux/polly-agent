a3b5a078493f89352b7d369a504dbc13517db335
Surya Govindasamy Ganesan <suryaganesan925@gmail.com>
2024-12-25 03:18:19 +0000

    updated ip

--- app/components/ChatBot.tsx
+++ app/components/ChatBot.tsx
@@ -32,7 +32,7 @@ const ChatBot: React.FC = () => {
         { sender: "user", msg: message },
       ];
 
-      const response = await fetch("http://0.0.0.0:8000/api/chat/", {
+      const response = await fetch("http://127.0.0.1:8000/api/chat/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
